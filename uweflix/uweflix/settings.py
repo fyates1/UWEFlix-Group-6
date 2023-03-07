5c56c32908e2fbe5e8740da357c139191a6194c6
Shuaib <shuaib3219@gmail.com>
2023-03-07 15:35:44 +0000

    Changed the database to SQLLite temporarily

--- uweflix/uweflix/settings.py
+++ uweflix/uweflix/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'uwe.apps.UweConfig',
-    'accounts.app.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'cinema.apps.CinemaConfig',
+    'clubRep.apps.ClubrepConfig',
+    'customer.apps.CustomerConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,14 +78,21 @@ WSGI_APPLICATION = 'uweflix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'uweflixdb',
+#         'USER': 'postgres',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'uweflixdb',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
