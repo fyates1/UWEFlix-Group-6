335a51c47244217e7cf7eff4e09fee8df4eac2a0
Shuaib <shuaib3219@gmail.com>
2023-03-07 14:21:42 +0000

    Added the other apps, modified the url.py from all apps

--- uweflix/uwe/urls.py
+++ uweflix/uwe/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('superuser/', views.superuser, name='superuser'),
-    path('accountManager/', views.accountManager, name='accountManager'),
-    path('clubRepresentative/', views.clubRepresentative, name='clubRepresentative'),
-    path('customer/', views.customer, name='customer'),
-    path('cinemaManager/', views.cinemaManager, name='cinemaManager'),
+    # path('accountManager/', views.accountManager, name='accountManager'),
+    # path('clubRepresentative/', views.clubRepresentative, name='clubRepresentative'),
+    # path('customer/', views.customer, name='customer'),
+    # path('cinemaManager/', views.cinemaManager, name='cinemaManager'),
 ]
--- uweflix/uweflix/settings.py
+++ uweflix/uweflix/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'uwe.apps.UweConfig'
+    'uwe.apps.UweConfig',
+    'accounts.app.AccountsConfig'
 ]
 
 MIDDLEWARE = [
