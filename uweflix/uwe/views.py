6b17fb1adea0d49eb4455f0277a47039ae0e4029
Shuaib <shuaib3219@gmail.com>
2023-03-20 23:50:08 +0000

    Merged User implementations, modified register and log. Login has yet to be tested but register is done

--- uweflix/uwe/views.py
+++ uweflix/uwe/views.py
@@ -1,51 +1,102 @@
 from django.shortcuts import render, redirect
 from .forms import *
 from django.contrib.auth import authenticate, login
-from .models import *
+from accounts.models import *
+from django.urls import reverse
 
-# Create your views here.
-
-#REGISTER
+# ----------------- Views -----------------
+# Register page
 def register(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('loginView')
+    # Creates the UserForm based on if .POST exists
+    form = UserForm(request.POST or None)
+
+    # Gets all the variables to be passed into the page
+    context = {
+        "form": form
+    }
+
+    # Checks if it's POST method
+    if request.method != 'POST':
+        return render(request, 'uwe/register.html', context)
+
+    # Checks if the form is valid
+    if form.is_valid():
+        # Saves the inputs to the database
+        user = form.save()
+        # Returns to the login page with a message
+        return redirect(reverse('loginView') + '?message=Registration Successful')
     else:
-        form = RegisterForm()
-    return render(request, 'uwe/register.html', {'form': form})
+        return render(request, 'uwe/register.html', context)
+
 #LOGIN
-def loginView(request):
-    form=LoginForm(request.POST or None)
-    if request.method == 'POST':
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            print(username,password)
-            user = authenticate(username=username, password=password)
-            print(user)
-            if user is not None and user.is_superuser:
-                login(request, user)
-                return redirect('superuser')
-            elif user is not None and user.is_accountManager:
-                login(request, user)
-                return redirect('accountManager')
-            elif user is not None and user.is_clubRepresentative:
-                login(request, user)
-                return redirect('clubRepresentative')
-            elif user is not None and user.is_customer:
-                login(request, user)
-                return redirect('customer')
-            elif user is not None and user.is_cinemaManager:
-                login(request, user)
-                return redirect('cinemaManager')
-            else:
-                return redirect('loginView')
+def login(request):
+    # TODO Needs to be tested
+
+    # Creates the UserForm based on if .POST exists
+    form = UserForm(request.POST or None)
+
+    # Gets the message from the query parameters
+    message = request.GET.get('message', None)
+
+    # Gets all the variables to be passed into the page
+    context = {
+        'form': form,
+        'message': message
+    }
+
+    # Checks if it's POST method
+    if request.method == 'POST' and form.is_valid():
+        # Gets the username and password inputs from the form
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+
+        # Authenticates the user
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            # Logs in the user and sets the user type in the session
+            login(request, user)
+            request.session['user_type'] = user.getUserType()
+
+            # Redirects to the home page
+            return redirect('home')
         else:
-            return redirect('loginView')
-    return render(request, 'uwe/login.html', {'form': form})
-    
+            # Adds an error message to the context and renders the page again
+            context['error'] = 'Invalid username or password'
+            return render(request, 'uwe/login.html', context)
+
+    # Renders the page
+    return render(request, 'uwe/login.html', context)
+
+# def loginView(request):
+#     form=LoginForm(request.POST or None)
+#     if request.method == 'POST':
+#         if form.is_valid():
+#             username = form.cleaned_data.get('username')
+#             password = form.cleaned_data.get('password')
+#             print(username,password)
+#             user = authenticate(username=username, password=password)
+#             print(user)
+#             if user is not None and user.is_superuser:
+#                 login(request, user)
+#                 return redirect('superuser')
+#             elif user is not None and user.is_accountManager:
+#                 login(request, user)
+#                 return redirect('accountManager')
+#             elif user is not None and user.is_clubRepresentative:
+#                 login(request, user)
+#                 return redirect('clubRepresentative')
+#             elif user is not None and user.is_customer:
+#                 login(request, user)
+#                 return redirect('customer')
+#             elif user is not None and user.is_cinemaManager:
+#                 login(request, user)
+#                 return redirect('cinemaManager')
+#             else:
+#                 return redirect('loginView')
+#         else:
+#             return redirect('loginView')
+#     return render(request, 'uwe/login.html', {'form': form})
 
 #USERS
 def superuser(request):
