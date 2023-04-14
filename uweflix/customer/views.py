9841d5ee008b1d9cd5e06df4ba37b25b7ace97b0
HarounEr <haroun.jo21@gmail.com>
2023-04-14 17:25:50 +0100

    added condition for stripe depending on booking form + self-checkout

--- uweflix/customer/views.py
+++ uweflix/customer/views.py
@@ -38,15 +38,30 @@ def landing(response):
 # sucess page where the booking gets saved
 @csrf_exempt
 def sucess(request):
+    #id = request.session['id']
+    id = 3
+    if id == 1:
+        adult_tickets = request.session['adult']
+        student_tickets = request.session['student']
+        child_tickets = request.session['child']
+        showing_id = request.session['showing_info']
+        showing = CinemaManager.get_showing(showing_id)
+        booking = Booking(showing=showing, student_tickets=student_tickets, child_tickets=child_tickets, adult_tickets=adult_tickets) #, total_price=total_price)
+        booking.save()
+        request.session['boooking_id'] = str(booking.bookingID)
+        del request.session['id']
+    elif id ==2 :
+        cr = request.session['cr']
+        showing_id = request.session['showing_info']
+        showing = CinemaManager.get_showing(showing_id)
+        booking = Booking(showing=showing, cr_tickets=cr)#, child_tickets=child_tickets, adult_tickets=adult_tickets) #, total_price=total_price)
+        booking.save()
+        request.session['boooking_id'] = str(booking.bookingID)
+        del request.session['id']
+    else:
+        pass
+        #return voew balance.
 
-    adult_tickets = request.session['adult']
-    student_tickets = request.session['student']
-    child_tickets = request.session['child']
-    showing_id = request.session['showing_info']
-    showing = CinemaManager.get_showing(showing_id)
-    booking = Booking(showing=showing, student_tickets=student_tickets, child_tickets=child_tickets, adult_tickets=adult_tickets) #, total_price=total_price)
-    booking.save()
-    request.session['boooking_id'] = str(booking.bookingID)
     return render(request, "customer/sucess.html")
 
 @csrf_exempt
@@ -56,191 +71,124 @@ def sendmaill(request):
 
 # cancel page
 @csrf_exempt
-def cancel(response):
-    pass
-    return render(response, "customer/cancel.html")
+def cancel(request):
+    id = request.session['id']
+    if id == 1:
+        del request.session['adult']
+        del request.session['student']
+        del request.session['child']
+        del request.session['id']
+    else:
+        del request.session['cr']
+        del request.session['id']
+
+   
+    
+    return render(request, "customer/cancel.html")
 
 # stripe payment
 @csrf_exempt
+
 def pay(request):
     #booking = Booking.objects.get(pk=booking_id)
-    adults = request.session['adult']
-    student = request.session['student']
-    child = request.session['child']
-    #DOMAIN ='http://127.0.0.1:8000/customer/',
-    items=[]
-    if(adults > 0):
-        items += [{
-            "price": "price_1MrLtCKummhyRPIWtVsccm4O",
-            "quantity": adults,
-            }]
-    if(student > 0 ):
-        items += [{
-            "price": "price_1MrLtaKummhyRPIWQbNz4wn6",
-            "quantity": student,
-            }]
-    if(child > 0 ):
-        items += [{
-            "price": "price_1MrLvzKummhyRPIW8U9BAMmJ",
-            "quantity": child,
-            }]
-    
-    checkout_session = stripe.checkout.Session.create(
-        payment_method_types=['card'], 
-        line_items=items,   
-        mode='payment',
-        success_url= 'http://127.0.0.1:8000/customer/sucess/', # will be changed later to proper url
-        cancel_url= 'http://127.0.0.1:8000/customer/cancel/',
+    #id = request.session['id']
+    id = 3
+    if id == 1:
+
+        adults = request.session['adult']
+        student = request.session['student']
+        child = request.session['child']
+        #cr = request.session['cr']
+
+        #DOMAIN ='http://127.0.0.1:8000/customer/',
+        items=[]
+        if(adults > 0):
+            items += [{
+                "price": "price_1MrLtCKummhyRPIWtVsccm4O",
+                "quantity": adults,
+                }]
+        if(student > 0 ):
+            items += [{
+                "price": "price_1MrLtaKummhyRPIWQbNz4wn6",
+                "quantity": student,
+                }]
+        if(child > 0 ):
+            items += [{
+                "price": "price_1MrLvzKummhyRPIW8U9BAMmJ",
+                "quantity": child,
+                }]
+        # if(cr > 0 ):
+        #     items+= [{
+        #         "price": "price_1MwnZwKummhyRPIWTUSuVw01",
+        #         "quantity": cr,
+        #         }]
+        # del request.session['adult']
+        # del request.session['student']
+        # del request.session['child']
+        # del request.session['cr']
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'], 
+            line_items=items,   
+            mode='payment',
+            success_url= 'http://127.0.0.1:8000/customer/sucess/', # will be changed later to proper url
+            cancel_url= 'http://127.0.0.1:8000/customer/cancel/',
+            
+            )
+        return redirect(checkout_session.url)
+
+    elif id == 2:
+
+
+        cr = request.session['cr']
+
+        #DOMAIN ='http://127.0.0.1:8000/customer/',
+        items=[]
+
+        if(cr > 0 ):
+            items+= [{
+                "price": "price_1MwnZwKummhyRPIWTUSuVw01",
+                "quantity": cr,
+                }]
+        # del request.session['adult']
+        # del request.session['student']
+        # del request.session['child']
+        #del request.session['cr']
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'], 
+            line_items=items,   
+            mode='payment',
+            success_url= 'http://127.0.0.1:8000/customer/sucess/', # will be changed later to proper url
+            cancel_url= 'http://127.0.0.1:8000/customer/cancel/',
+            
+            )
+        return redirect(checkout_session.url)
+    else:
+        #cr = request.session['cr']
+
+        #DOMAIN ='http://127.0.0.1:8000/customer/',
+        items=[]
+
         
-        )
-    return redirect(checkout_session.url)
+        items+= [{
+            "price": "price_1MwospKummhyRPIWFz1IxOIy",
+            "quantity": 1,
+            }]
+        # del request.session['adult']
+        # del request.session['student']
+        # del request.session['child']
+        #del request.session['cr']
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'], 
+            line_items=items,   
+            mode='payment',
+            success_url= 'http://127.0.0.1:8000/customer/sucess/', # will be changed later to proper url
+            cancel_url= 'http://127.0.0.1:8000/customer/cancel/',
+            
+            )
+        return redirect(checkout_session.url)
 
 #testing card details for stripe
 #number : 4242 4242 4242 4242
 # 04/23 
 # 123
 #
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from django.shortcuts import render
-# from datetime import datetime
-
-# from django.contrib.auth import authenticate
-# from django.contrib.auth.decorators import login_required
-# from django.shortcuts import render, redirect
-
-# # Create your views here.
-# from django.http import HttpResponse
-# from django.views.generic import TemplateView
-
-# from .models import Theatre, TicketBooking
-# from django.db.models import Q
-# from .models import ShowingItem
-# from .forms import TicketBookingFormCreditCard, TicketBookingFormClub
-
-# # Create your views here.
-
-
-# class BookTicket(TemplateView):
-#     template_name = "ticket_booking.html"
-
-#     def post(self, request, *args, **kwargs):
-#         print (request.POST)
-#         print(args)
-#         print(kwargs)
-
-#         current_user = self.request.user
-#         print(current_user.role)
-#         if current_user.role == 2:
-#             ticket_data = TicketBookingFormClub(request.POST)
-#             if ticket_data.is_valid():
-#                 print("data valid")
-#                 show = showing.objects.get(id=kwargs['show_item'])
-#                 ticket_date = datetime.strptime(request.POST["ticket_date"], '%d/%m/%Y')
-#                 tickets = TicketBooking.objects.filter(showing=show, ticket_date=datetime.strftime(ticket_date, "%Y-%m-%d"))
-#                 total = 0
-#                 for tic in tickets:
-#                     total = total + int(tic.ticket_count)
-
-#                 print(total)
-
-#                 if total + int(request.POST["number_of_tickets"]) > show.total_tickets:
-#                     return HttpResponse("Not enough tickets , please try with a smaller number")
-#                 user = authenticate(username=request.POST["user_name"], password=request.POST["pass_word"])
-#                 if user is None:
-#                     return HttpResponse("Incorrect credentials")
-#                 my_ticket = TicketBooking()
-#                 my_ticket.ticket_date = ticket_date
-#                 my_ticket.ticket_type = 1
-#                 my_ticket.ticket_count = request.POST["number_of_tickets"]
-#                 my_ticket.buyer = self.request.user
-#                 my_ticket.status = "ok"
-#                 my_ticket.showing = show
-#                 my_ticket.save()
-#                 return HttpResponse("Data submitted successfully")
-
-#             else:
-#                 context = self.get_context_data(**kwargs)
-#                 context['form'] = ticket_data
-#                 return self.render_to_response(context)
-
-#         else:
-#             ticket_data = TicketBookingFormCreditCard(request.POST)
-#             if ticket_data.is_valid():
-#                 #check for number of tickets
-#                 print("data valid")
-#                 show = ShowingItem.objects.get(id=kwargs['show_item'])
-#                 ticket_date = datetime.strptime(request.POST["ticket_date"], '%d/%m/%Y')
-#                 tickets = TicketBooking.objects.filter(showing=show,
-#                                                        ticket_date=datetime.strftime(ticket_date, "%Y-%m-%d"))
-#                 total = 0
-#                 for tic in tickets:
-#                     total = total + int(tic.ticket_count)
-
-#                 if total + int(request.POST["number_of_tickets"]) > show.total_tickets:
-#                     return HttpResponse("Not enough tickets , please try with a smaller number")
-
-#                 my_ticket = TicketBooking()
-#                 my_ticket.ticket_date = ticket_date
-#                 my_ticket.ticket_type = request.POST["ticket_type"]
-#                 my_ticket.ticket_count = request.POST["number_of_tickets"]
-#                 my_ticket.buyer = self.request.user
-#                 my_ticket.status = "ok"
-#                 my_ticket.showing = show
-#                 my_ticket.credit_card_info = request.POST["credit_card_info"]
-#                 my_ticket.credit_card_name = request.POST["credit_card_name"]
-#                 my_ticket.credit_card_exp = request.POST["credit_card_year"] + "/" + request.POST["credit_card_month"]
-#                 my_ticket.save()
-#                 return HttpResponse("Data submitted successfully")
-#             else:
-#                 print("data invalid")
-
-#                 context = self.get_context_data(**kwargs)
-#                 context['form'] = ticket_data
-#                 return self.render_to_response(context)
-
-#             return HttpResponse("Data submitted successfully")
-
-#         context = self.get_context_data(**kwargs)
-#         context['form'] = ticket_data
-#         return self.render_to_response(context)
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-
-#         current_user = self.request.user
-#         print(current_user.role)
-#         if current_user.role == 2:
-#             context['form'] = TicketBookingFormClub()
-#             context['action'] = "confirm_club"
-#         else:
-#             context['form'] = TicketBookingFormCreditCard()
-#             context['action'] = "confirm"
-
-#         return context
