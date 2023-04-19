5f95aed3e0a47e5990aedc1c7ff5881269a4fb00
KTComputing123 <72080149+KTComputing123@users.noreply.github.com>
2023-04-19 11:56:53 +0100

    cancel bookings

--- uweflix/uwe/urls.py
+++ uweflix/uwe/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     
     path('contact_us', views.contact_us, name='contact_us'),
     path('my_tickets', views.my_tickets, name='my_tickets'),
+    path('view_bookings', views.view_bookings, name='view_bookings'),
+    path('cancel_bookings/<str:pk>/', views.cancel_bookings, name='cancel_bookings'),
 ]
